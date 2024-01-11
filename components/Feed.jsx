7eb3f0b6180a6bfc989f41ba8c7441b62f0890ae
Viren Khatri <vk102002@gmail.com>
2024-01-11 23:54:45 +0530

    fix: feed not showing all posts

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -26,6 +26,7 @@ const Feed = () => {
     const fetchPosts = async () => {
       const response = await fetch("/api/prompt");
       const data = await response.json();
+      // console.log(data);
       setAllPosts(data);
     };
     fetchPosts();
